c7343b267f03a7b3cece0e2e4afcbf54f6571731
gustaoliv <gusta.oliv4@gmail.com>
2022-01-14 17:39:01 -0300

    criação das tabelas no modelo

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    id_region = models.ForeignKey(Region, on_delete=models.CASCADE)
